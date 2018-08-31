8f8787e08b1f8e21d4d16697465bd9fd43c0d53a
Morgan Newman <mpnewman95@gmail.com>
2018-08-31 13:53:50 -0700

    Converted images to SVG

--- scripts/list.js
+++ scripts/list.js
@@ -29,7 +29,7 @@ const list = (function() {
     return `
     <div data-id="${bookmark.id}" class="list-container list-container-condensed">
       <div class="list-item">
-        <div class="fas fa-angle-down fa-lg list-item-expander"></div>
+        <div class="list-item-expander"><svg aria-hidden="true" data-prefix="fas" data-icon="angle-down" class="svg-inline--fa fa-angle-down fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z"></path></svg></div>
         <h3 class="list-item-title">${bookmark.title}</h3>
         <div class="list-item-container">
           <div class="list-item-stars">
@@ -45,16 +45,16 @@ const list = (function() {
     // TODO: Change button over to link. Must fix styling.
     return `
       <div data-id="${bookmark.id}" class="list-container list-container-expanded">
-        <div class="fas fa-angle-up fa-lg list-item-condenser"></div>
+        <div class="list-item-condenser"><svg aria-hidden="true" data-prefix="fas" data-icon="angle-up" class="svg-inline--fa fa-angle-up fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M177 159.7l136 136c9.4 9.4 9.4 24.6 0 33.9l-22.6 22.6c-9.4 9.4-24.6 9.4-33.9 0L160 255.9l-96.4 96.4c-9.4 9.4-24.6 9.4-33.9 0L7 329.7c-9.4-9.4-9.4-24.6 0-33.9l136-136c9.4-9.5 24.6-9.5 34-.1z"></path></svg></div>
         <h3 class="list-item-title list-item-title-expanded">${bookmark.title}</h3>
         <div class="list-item-container list-item-container-expanded">
           <p class="list-item-description">${bookmark.desc}</p>
           <div class="list-item-expanded-bottom-items">
-            <button href="${bookmark.url}" target="_blank" class="button-primary">Visit Site</button>
+            <a href="${bookmark.url}" target="_blank" class="button-primary button-primary-link">Visit Site</a>
             <div class="list-item-stars list-item-stars-expanded">
               ${generateStars(bookmark.rating)}
           </div>
-          <span data-id="${bookmark.id}" class="js-delete-item list-item-delete fas fa-trash-alt fa-lg"></span>
+          <div data-id="${bookmark.id}" class="js-delete-item list-item-delete"><svg aria-hidden="true" data-prefix="fas" data-icon="trash-alt" class="svg-inline--fa fa-trash-alt fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M0 84V56c0-13.3 10.7-24 24-24h112l9.4-18.7c4-8.2 12.3-13.3 21.4-13.3h114.3c9.1 0 17.4 5.1 21.5 13.3L312 32h112c13.3 0 24 10.7 24 24v28c0 6.6-5.4 12-12 12H12C5.4 96 0 90.6 0 84zm416 56v324c0 26.5-21.5 48-48 48H80c-26.5 0-48-21.5-48-48V140c0-6.6 5.4-12 12-12h360c6.6 0 12 5.4 12 12zm-272 68c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208zm96 0c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208zm96 0c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208z"></path></svg></div>
         </div>
       </div>
     </div>
@@ -64,7 +64,7 @@ const list = (function() {
   const generateBookmarkCards = function() {
     return store.state.items.reduce((bookmarks, item) => {
       // If the item is at the current filter level or higher...
-      if (item.rating >= store.currentFilterLevel()) {
+      if (store.currentFilterLevel() === 0 || item.rating === store.currentFilterLevel()) {
         // Render the correct expanded/condensed state
         if (item.expanded) bookmarks.push( generateExpandedCardHTML(item));
         else bookmarks.push(generateCondensedCardHTML(item));
